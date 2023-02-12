487d0e1da9ffcfdccf9a8d01b641535b8e236218
Mariia Prokopenko <Klymenkomasha@gmail.com>
2023-02-12 13:07:20 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 // import React, { Component } from 'react';
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import FeedbackClick from './FeedbackClick/FeedbackClick';
 import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
